94ffb47932b05ddf9d78a28a544be7316983b5cc
Imad Mohamad <yehya.mh@live.com>
2024-05-11 03:28:08 +0200

    Add exercise 3

---
+++ Exercise3.py
@@ -0,0 +1,18 @@
+
+# exercise 3.1
+red_objects ={"apple", "crab", "rose", "strawberry"}
+fruits = {"orange", "apple", "strawberry", "grape", "kiwi", "mandarin"}
+
+print(f"the red fruits is {fruits & red_objects}")
+print(f"all fruits that aren’t red fruits is {fruits - red_objects}")
+
+
+# exercise 3.2
+orange_objects ={"basketball", "fanta", "orange", "autumn leaves", "mandarin"}
+
+print(f"all of the red and orange fruits but none of the other ones is {(fruits & red_objects) | (fruits & orange_objects)}")
+print(f"all of the objects without the fruits is {(red_objects - fruits) | (orange_objects - fruits)}")
+
+
+# exercise 3.2
+print(f"all of the objects and fruits is {list(red_objects  | orange_objects | fruits)}")
