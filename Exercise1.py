352521e6cbfa9157f2b092f2b63e85540f767b19
Imad Mohamad <yehya.mh@live.com>
2024-05-11 03:53:10 +0200

    Modified Exercice 1

--- Exercise1.py
+++ Exercise1.py
@@ -1,18 +1,12 @@
 
 
-#change type from string to int.
-def ChangeStrToInt(lst):
-    for i, j in enumerate(lst):
-        if j.isnumeric():
-            lst[i]= int(j)
+set_1 = {1, 2, 3, 4}
+set_2 =  {7, 8, 9, 10}
 
-list_1 = ChangeStrToInt(input("Enter the first set\n"
-                    +"'put a space between them' :").split())
-list_2 = ChangeStrToInt(input("Enter the second set\n"
-                    +"'put a space between them' :").split())
-
-set_1 = set(list_1)
-set_2 = set(list_2)
+list_1 = list(set_1)
+list_2 = list(set_2)
+list_1.sort()
+list_2.sort()
 
 new_dict = {list_1[i]: list_2[i] for i in range(len(list_1))}
 
