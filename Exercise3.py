14fda978e4956db7ac54ece65c29b54978afc930
Imad Mohamad <yehya.mh@live.com>
2024-05-12 01:54:58 +0200

    Modified sone commands sorted

--- Exercise3.py
+++ Exercise3.py
@@ -10,9 +10,9 @@ print(f"all fruits that aren’t red fruits is {fruits - red_objects}")
 # exercise 3.2
 orange_objects ={"basketball", "fanta", "orange", "autumn leaves", "mandarin"}
 
-print(f"all of the red and orange fruits but none of the other ones is {(fruits & red_objects) | (fruits & orange_objects)}")
-print(f"all of the objects without the fruits is {(red_objects - fruits) | (orange_objects - fruits)}")
+print(f"all of the red and orange fruits but none of the other ones is {fruits & (red_objects | orange_objects)}")
+print(f"all of the objects without the fruits is {(red_objects | orange_objects) - fruits}")
 
 
 # exercise 3.2
-print(f"all of the objects and fruits is {list(red_objects  | orange_objects | fruits)}")
+print(f"all of the objects and fruits is {list(red_objects | orange_objects | fruits)}")
